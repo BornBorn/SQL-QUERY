5e9f35c1766d21f3df098789ad0378104754b038
bornborn <voracious51806@gmail.com>
2016-11-03 00:54:14 +0800

    Activity

--- dbo.ActivityLog.sql
+++ dbo.ActivityLog.sql
@@ -1,8 +1,7 @@
 ﻿CREATE TABLE [dbo].[ActivityLog] (
     [details]    VARCHAR (100) NOT NULL,
-    [date]       DATE          NULL,
-    [time]       TIME (0)      NULL,
-    [staff_name] VARCHAR (50)  NULL, 
-    [staff_id] VARCHAR(50) NULL
+    [date]       DATETIME      NULL,
+    [staff_name] VARCHAR (50)  NULL,
+    [staff_id]   VARCHAR (50)  NULL
 );
 
