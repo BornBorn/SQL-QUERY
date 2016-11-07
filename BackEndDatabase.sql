e0d95843f71ea26765cef4a98902eb2cb3339dac
bornborn <voracious51806@gmail.com>
2016-11-07 23:15:45 +0800

    customer database

--- BackEndDatabase.sql
+++ BackEndDatabase.sql
@@ -118,3 +118,20 @@ CREATE TABLE [dbo].[Supplier] (
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
+CREATE TABLE [dbo].[Customer] (
+    [id]              VARCHAR (50)  NOT NULL,
+    [name]            VARCHAR (50)  NULL,
+    [Password]        NVARCHAR (20) NOT NULL,
+    [email]           VARCHAR (50)  NULL,
+    [phone]           VARCHAR (50)  NULL,
+    [nric]            VARCHAR (50)  NULL,
+    [address]         VARCHAR (MAX) NULL,
+    [gender]          VARCHAR (50)  NULL,
+    [created_date]    DATETIME      NOT NULL,
+    [last_login_date] DATETIME      NULL,
+    [reward_points]   INT           NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
+
+
