b49fdff5f4aa9f6def3aaddf6d0b915e497a101e
bornborn <voracious51806@gmail.com>
2016-11-07 22:56:10 +0800

    OrderTracking Database

---
+++ dbo.OrderTracking.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[OrderTracking] (
+    [stock_id] VARCHAR (50) NOT NULL,
+    [order_id] VARCHAR (50) NULL,
+    [received] INT          NULL,
+    [defect]   INT          NULL
+);
+
