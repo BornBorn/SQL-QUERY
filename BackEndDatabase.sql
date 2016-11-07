ba0218e53eb1c82444cde63135ea273ca7d99e0e
bornborn <voracious51806@gmail.com>
2016-11-07 23:25:18 +0800

    Updated service and backend database

--- BackEndDatabase.sql
+++ BackEndDatabase.sql
@@ -73,7 +73,7 @@ CREATE TABLE [dbo].[Service] (
     [id]       VARCHAR (50) NOT NULL,
     [price]    DECIMAL (18) NULL,
     [name]     VARCHAR (50) NULL,
-    [duration] TIME (0)     NULL,
+    [duration] INT     NULL,
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
--- dbo.Service.sql
+++ dbo.Service.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Service] (
-    [id]    VARCHAR (50) NOT NULL,
-    [price] DECIMAL (18) NULL,
-    [name]  VARCHAR (50) NULL,
-    [duration] TIME(0) NULL, 
+    [id]       VARCHAR (50) NOT NULL,
+    [price]    DECIMAL (18) NULL,
+    [name]     VARCHAR (50) NULL,
+    [duration] INT     NULL,
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
