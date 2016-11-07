5e993f8eb4fa3547883a7a9c6d962663a8553c72
bornborn <voracious51806@gmail.com>
2016-11-07 22:58:54 +0800

    Removed AppointmentService Foreign Key

--- dbo.AppointmentService.sql
+++ dbo.AppointmentService.sql
@@ -1,7 +1,6 @@
 ﻿CREATE TABLE [dbo].[AppointmentService] (
-    [appointment_id]   VARCHAR (50) NOT NULL,
-    [service_id]       VARCHAR (50) NOT NULL,
-    [service_name]     VARCHAR (50) NULL,
-    FOREIGN KEY ([service_id]) REFERENCES [dbo].[Service] ([id])
+    [appointment_id] VARCHAR (50) NOT NULL,
+    [service_id]     VARCHAR (50) NOT NULL,
+    [service_name]   VARCHAR (50) NULL
 );
 
